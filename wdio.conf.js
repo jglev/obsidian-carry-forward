08945f7a497e3e43df81bad19160baba1ee1bb74
Jacob Levernier <j@adunumdatum.org>
2022-02-20 12:13:42 -0500

    Handled chromedriver version with wdio-chromedriver-service

--- wdio.conf.js
+++ wdio.conf.js
@@ -114,11 +114,11 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [['chromedriver', {
-        args: {
-            version: "89",
-        },
-    }]],
+    // services: [['chromedriver', {
+    //     args: {
+    //         version: "89",
+    //     },
+    // }]],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
