b909dea33e21bbc4669a239c5a5aef154958a417
Jacob Levernier <j@adunumdatum.org>
2022-02-23 19:59:11 -0500

    Wrote test.

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,6 +1,9 @@
 const { assert } = require("chai");
 const fs = require("fs");
 
+// Note: Supported keyboard key names can be found here:
+// https://w3c.github.io/webdriver/webdriver-spec.html#keyboard-actions
+
 async function sleep(seconds) {
     return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
@@ -51,11 +54,53 @@ describe("Plugin", function () {
         await sleep(1);
     });
 
-    it("adds command", async function () {
-        await browser.$('.workspace').keys(['Control', 'p']);
+    it("adds commands", async function () {
+        await browser.$('.view-content').keys(['Control', 'p']);
         await sleep(0.5);
         await browser.$(".prompt-input").keys("Carry Forward");
         const commands = (await browser.$$(".suggestion-item")).length;
         assert(commands === 12, 'Wrong number of commands in Command Palette');
+    });
+
+    it("correctly copies the contents of all four lines in a block", async function () {
+        const testText = `- Lorem ipsum 1
+    - Lorem ipsum 2
+        - Lorem ipsum 3
+    - Lorem ipsum 4
+`;
+        await browser.$('.view-content').click();
+        await browser.$('.view-content').keys(testText);
+
+        // Select text:
+        await browser.$('.CodeMirror-line').keys(['ArrowUp']);
+        await browser.$('.CodeMirror-line').keys(['End']);
+        await browser.$('.CodeMirror-line').keys(['Shift', 'ArrowUp']);
+        await browser.$('.CodeMirror-line').keys(['Shift', 'ArrowUp']);
+        await browser.$('.CodeMirror-line').keys(['Shift', 'ArrowUp']);
+        await browser.$('.CodeMirror-line').keys(['Shift', 'Home']);
+
+        await browser.$('.view-content').keys(['Control', 'p']);
+        await sleep(1);
+        await browser.$(".prompt-input").keys("Carry Forward: Copy selection with each line linked to its copied source (default link text)");
+        await browser.$(".suggestion-item").click();
+        await sleep(1);
+
+        // Check the contents of the clipboard:
+        await $('.view-content').click({ button: "right" });
+        await sleep(1);
+        await $$('.menu-item')[4].click(); // "Select all"
+
+        await $('.view-content').click({ button: "right" });
+        await sleep(1);
+        await $$('.menu-item')[4].click(); // "Paste"
+
+        const pastedText = await $('.view-content').getText();
+
+        console.log(101, pastedText);
+
+        const testTextSplit = testText.split(/\r?\n/);
+        const pastedTextSplit = pastedText.split(/\r?\n/);
+
+        assert(pastedTextSplit[0 + 1].startsWith(testTextSplit[0]), `Line 0 ("${testTextSplit[0]}") is not the same ("${pastedTextSplit[0 + 1]}")`);
     });
 });
